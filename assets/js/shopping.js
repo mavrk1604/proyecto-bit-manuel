c0fae4c7d65511f2943f6f91395a7753eb4aa9f1
Mavrk1604 <mavrk1604@gmail.com>
2024-06-24 21:55:09 -0500

    imagenes de js indexadas

--- assets/js/shopping.js
+++ assets/js/shopping.js
@@ -1,17 +1,17 @@
 const shoppingcards = document.querySelector('#itemscards');
 const items = [
-  { id: 0, name: 'Tomate', type: 'Fruta', image: '/assets/img/ingredients/tomato.png', description: 'El tomate es el fruto de la planta Solanum lycopersicum, el cual tiene importancia culinaria, y es una verdura.'},
-  { id: 1, name: 'Cebolla', type: 'Verdura', image: '/assets/img/ingredients/onion.png'},
-  { id: 2, name: 'Ajo', type: 'Verdura', image: '/assets/img/ingredients/garlic.png'},
-  { id: 3, name: 'Manzana', type: 'Fruta', image: '/assets/img/ingredients/apple.png'},
-  { id: 4, name: 'Espinaca', type: 'Verdura', image: '/assets/img/ingredients/spinach.png'},
-  { id: 5, name: 'Rugula', type: 'Verdura', image: '/assets/img/ingredients/rugula.png' },
-  { id: 6, name: 'Banano', type: 'Fruta', image: '/assets/img/ingredients/banana.png' },
-  { id: 7, name: 'Naranja', type: 'Fruta', image: '/assets/img/ingredients/orange.png' },
-  { id: 8, name: 'Fresa', type: 'Fruta', image: '/assets/img/ingredients/strawberry.png' },
-  { id: 9, name: 'Uva', type: 'Fruta', image: '/assets/img/ingredients/grapes.png' },
-  { id: 10, name: 'Pera', type: 'Fruta', image: '/assets/img/ingredients/pear.png' },
-  { id: 11, name: 'Zanahoria', type: 'Verdura', image: '/assets/img/ingredients/carrot.png' },
+  { id: 0, name: 'Tomate', type: 'Fruta', image: './assets/img/ingredients/tomato.png', description: 'El tomate es el fruto de la planta Solanum lycopersicum, el cual tiene importancia culinaria, y es una verdura.'},
+  { id: 1, name: 'Cebolla', type: 'Verdura', image: './assets/img/ingredients/onion.png'},
+  { id: 2, name: 'Ajo', type: 'Verdura', image: './assets/img/ingredients/garlic.png'},
+  { id: 3, name: 'Manzana', type: 'Fruta', image: './assets/img/ingredients/apple.png'},
+  { id: 4, name: 'Espinaca', type: 'Verdura', image: './assets/img/ingredients/spinach.png'},
+  { id: 5, name: 'Rugula', type: 'Verdura', image: './assets/img/ingredients/rugula.png' },
+  { id: 6, name: 'Banano', type: 'Fruta', image: './assets/img/ingredients/banana.png' },
+  { id: 7, name: 'Naranja', type: 'Fruta', image: './assets/img/ingredients/orange.png' },
+  { id: 8, name: 'Fresa', type: 'Fruta', image: './assets/img/ingredients/strawberry.png' },
+  { id: 9, name: 'Uva', type: 'Fruta', image: './assets/img/ingredients/grapes.png' },
+  { id: 10, name: 'Pera', type: 'Fruta', image: './assets/img/ingredients/pear.png' },
+  { id: 11, name: 'Zanahoria', type: 'Verdura', image: './assets/img/ingredients/carrot.png' },
 
 ];
 
